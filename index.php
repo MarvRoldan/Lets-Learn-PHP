2e88aff2c9a6992bd790a94094550eb465e5379b
MarvRoldan <marvinroldan_@hotmail.com>
2020-04-29 09:45:28 -0600

    Added data types

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
+    // Basic assignment.
     $myMessage = 'My First PHP!';
+    # This is also a comment!
+    /*
+      Multi-line
+      comments!
+    */
+
+    // Data-Types.
+    $myInteger = 54; // Integer
+    $myFloat = 38.968; // Float
+    $myBoolean = TRUE; //Boolean
+    $mySecondBoolean = false; // Lowercase works too!
+    $myString = 'Hello, World!'; // String
+    $myObject = new stdClass(); // Object
+    $myNull = NULL; // NULL
+    $myArray = array( $myInteger, $myFloat, $myBoolean, $mySecondBoolean, $myString, $myObject, $myNull ); // Array
 ?>
 <!DOCTYPE html>
 <html lang="en">
